0a33fb5496afbf6fdf7cc83d849fb24ac1f9d684
Daniel Kim <danielkim2711@gmail.com>
2021-05-31 00:01:10 +1200

    Game

--- js/main.js
+++ js/main.js
@@ -5,16 +5,25 @@ canvas.width = 1000;
 canvas.height = 600;
 
 const keys = [];
+const worms = [];
 
 const player = {
     x: 500,
-    y: 350,
+    y: 400,
     width: 40,
     height: 56,
     frameX: 0,
     frameY: 0,
     speed: 7,
     moving: false
+};
+
+const worm = {
+    x: 200,
+    y: 300,
+    radius: 15,
+    dx: 3,
+    dy: 2
 };
 
 const character = new Image();
@@ -24,6 +33,37 @@ background.src = "img/beach.png";
 
 function drawCharacter(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
+}
+
+function drawWorm() {
+    ctx.beginPath();
+    ctx.arc(worm.x, worm.y, worm.radius, Math.PI, 0);
+    ctx.fillStyle = 'rgb(250, 200, 120)';
+    ctx.fill();
+    ctx.lineWidth = 1.5;
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+}
+
+function drawScore() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, 250, 40);
+    ctx.font = '20px Arial';
+    ctx.fillStyle = '#000';
+    ctx.fillText("Score: ", 10, 25);
+    ctx.fillText("Time Left: ", 120, 25);
+}
+
+let num = 180;
+let intvl = setInterval(function () { timeLeft(num--); }, 1000);
+
+function timeLeft() {
+    ctx.font = "20px Arial";
+    ctx.fillStyle = "#000";
+    ctx.fillText(num, 210, 25);
+    if (num == 0) {
+        clearInterval(intvl);
+    }
 }
 
 window.addEventListener('keydown', function (e) {
@@ -56,19 +96,34 @@ function movePlayer() {
     }
 }
 
+function moveWorm() {
+    worm.x += worm.dx;
+    worm.y += worm.dy;
+
+    if (worm.x + worm.radius > canvas.width || worm.x - worm.radius < 0) {
+        worm.dx *= -1;
+    } else if (worm.y + worm.radius > canvas.height || worm.y - worm.radius < 250) {
+        worm.dy *= -1;
+    }
+}
+
 function handlePlayerFrame() {
     if (player.frameX < 3 && player.moving) {
         player.frameX++;
     } else player.frameX = 0;
 }
 
-function update() {
+function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     drawCharacter(character, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
+    drawWorm();
+    drawScore();
     movePlayer();
+    moveWorm();
+    timeLeft();
     handlePlayerFrame();
-    requestAnimationFrame(update);
+    requestAnimationFrame(animate);
 }
 
-update();
+animate();
