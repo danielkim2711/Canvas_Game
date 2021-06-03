5120468639c7720792c0899a9c4b8ca2d60c8a33
Daniel Kim <danielkim2711@gmail.com>
2021-06-03 17:21:04 +1200

    game

--- js/main.js
+++ js/main.js
@@ -4,45 +4,264 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1000;
 canvas.height = 600;
 
+
+let num = 180;
+let intvl;
+
+let isStart = false;
+
+const character = new Image();
+character.src = "img/ironman.png";
+const background = new Image();
+background.src = "img/beach.png";
+
 const keys = [];
-const worms = [];
 
 const player = {
     x: 500,
     y: 400,
-    width: 40,
-    height: 56,
+    width: 32,
+    height: 48,
     frameX: 0,
     frameY: 0,
-    speed: 5,
-    moving: false
+    speed: 3,
+    moving: false,
+    isColliding: false
 };
 
-const worm = {
-    x: 200,
-    y: 300,
-    radius: 15,
-    dx: 3,
-    dy: 2
-};
+let worms = [
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    },
+    {
+        x: 0,
+        y: 0,
+        radius: 10,
+        dx: 1,
+        dy: 1,
+        lc: 4,
+        time: 1,
+        delay: 0
+    }
+];
 
-const character = new Image();
-character.src = "img/hulk.png";
-const background = new Image();
-background.src = "img/beach.png";
+function drawWorm(worm) {
+    if (worm.delay <= 0) {
+        ctx.beginPath();
+        ctx.arc(worm.x + worm.radius, worm.y + worm.radius, worm.radius, Math.PI, 0);
+        ctx.fillStyle = 'rgb(250, 200, 120)';
+        ctx.fill();
+        ctx.lineWidth = 1.5;
+        ctx.strokeStyle = "black";
+        ctx.stroke();
+    }
+}
+
+function moveWorm() {
+    for (var i = 0; i < worms.length; i++) {
+        switch (worms[i].lc) {
+            case 1:
+                worms[i].x += worms[i].dx;
+                worms[i].y += worms[i].dy;
+                worms[i].time = num;
+                worms[i].lc = 2;
+                break;
+            case 2:
+                worms[i].x += worms[i].dx;
+                worms[i].y += worms[i].dy;
+                worms[i].radius += 0.05;
+
+                if (worms[i].time - num > 5) {
+                    worms[i].lc = 3;
+                    worms[i].time = num;
+                    console.log("change to 3");
+                }
+                break;
+            case 3:
+                worms[i].x += worms[i].dx;
+                worms[i].y += worms[i].dy;
+                worms[i].radius -= 0.05;
+
+                if (worms[i].radius < 10) {
+                    worms[i].radius = 10;
+                }
+
+                if (worms[i].time - num > 5) {
+                    reset(worms[i]);
+                }
+                break;
+            case 4:
+                worms[i].delay--;
+                console.log("case 4", worms[i].delay);
+
+                if (worms[i].delay <= 0) {
+                    worms[i].lc = 1;
+                }
+            default:
+                break;
+        }
+    }
+}
 
 function drawCharacter(img, sX, sY, sW, sH, dX, dY, dW, dH) {
     ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
-}
-
-function drawWorm() {
-    ctx.beginPath();
-    ctx.arc(worm.x, worm.y, worm.radius, Math.PI, 0);
-    ctx.fillStyle = 'rgb(250, 200, 120)';
-    ctx.fill();
-    ctx.lineWidth = 1.5;
-    ctx.strokeStyle = "black";
-    ctx.stroke();
 }
 
 function drawScore() {
@@ -52,17 +271,18 @@ function drawScore() {
     ctx.fillStyle = '#000';
     ctx.fillText("Score: ", 10, 25);
     ctx.fillText("Time Left: ", 120, 25);
+    timeLeft();
 }
-
-let num = 180;
-let intvl = setInterval(function () { timeLeft(num--); }, 1000);
 
 function timeLeft() {
     ctx.font = "20px Arial";
     ctx.fillStyle = "#000";
     ctx.fillText(num, 210, 25);
-    if (num == 0) {
+
+    if (num === 0) {
         clearInterval(intvl);
+        alert("Game Over");
+        num = "0";
     }
 }
 
@@ -74,6 +294,12 @@ window.addEventListener('keydown', function (e) {
 window.addEventListener('keyup', function (e) {
     delete keys[e.keyCode];
     player.moving = false;
+});
+
+canvas.addEventListener('mousedown', function (e) {
+    if (!isStart) {
+        start();
+    }
 });
 
 function movePlayer() {
@@ -100,17 +326,12 @@ function movePlayer() {
         player.y += player.speed;
         player.frameY = 0;
         player.moving = true;
-    }
-}
 
-function moveWorm() {
-    worm.x += worm.dx;
-    worm.y += worm.dy;
-
-    if (worm.x + worm.radius > canvas.width || worm.x - worm.radius < 0) {
-        worm.dx *= -1;
-    } else if (worm.y + worm.radius > canvas.height || worm.y - worm.radius < 250) {
-        worm.dy *= -1;
+        // Catch
+    } else if (keys[32]) {
+        player.frameY = 1;
+        player.moving = true;
+        checkObjectCollisions();
     }
 }
 
@@ -120,16 +341,387 @@ function handlePlayerFrame() {
     } else player.frameX = 0;
 }
 
+function getRandomInRange(min, max) {
+    return Math.random() * (max - Math.abs(min)) + min;
+}
+
+function reset(worm) {
+    worm.x = getRandomInRange(200, 800);
+    worm.y = getRandomInRange(300, 400);
+    worm.dx = getRandomInRange(0, 4) - 2;
+    worm.dy = getRandomInRange(0, 4) - 2;
+    worm.delay = getRandomInRange(20, 100);
+    worm.lc = 4;
+    console.log("reset", worm);
+}
+
+function checkCollisions() {
+    for (var i = 0; i < worms.length; i++) {
+        checkWallCollisions(i);
+    }
+}
+
+function checkWallCollisions(i) {
+    if (worms[i].x + worms[i].radius > canvas.width || worms[i].x - worms[i].radius < 0) {
+        worms[i].dx *= -1;
+        worms[i].x += worms[i].dx;
+    } else if (worms[i].y + worms[i].radius > canvas.height || worms[i].y - worms[i].radius < 250) {
+        worms[i].dy *= -1;
+        worms[i].y += worms[i].dy;
+    }
+}
+
+function checkObjectCollisions() {
+    for (var i = 0; i < worms.length; i++) {
+        var isCol = rectIntersect(
+            character.x,
+            character.y,
+            character.width,
+            character.height,
+            worms[i].x,
+            worms[i].y,
+            worms[i].width,
+            worms[i].height
+        );
+    }
+    if (isCol) {
+        reset(worms[i]);
+    }
+}
+
+function rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2) {
+    if (x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2) {
+        return false;
+    }
+    return true;
+}
+
+for (var i = 0; i < worms.length; i++) {
+    reset(worms[i]);
+}
+
 function animate() {
+    checkCollisions();
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     drawCharacter(character, player.width * player.frameX, player.height * player.frameY, player.width, player.height, player.x, player.y, player.width, player.height);
-    drawWorm();
+    moveWorm();
+    for (var i = 0; i < worms.length; i++) {
+        drawWorm(worms[i]);
+    }
     drawScore();
     movePlayer();
-    moveWorm();
     handlePlayerFrame();
     requestAnimationFrame(animate);
 }
 
-animate();
+function startBtn() {
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(400, 250, 200, 100);
+    ctx.fill();
+    ctx.font = '50px Arial';
+    ctx.fillStyle = '#000';
+    ctx.fillText("Start", 450, 320);
+    ctx.fill();
+}
+
+function pauseBtn() {
+    ctx.font = '50px Arial';
+    ctx.fillStyle = '#000';
+    ctx.fillText("Pause", 450, 320);
+    ctx.fill();
+    start();
+}
+
+function start() {
+    isStart = true;
+    intvl = setInterval(function () { timeLeft(num--); }, 1000);
+    animate();
+}
+
+startBtn();
+
+
+// function checkObjectCollisions(object, index) {
+//     // check parameter square against all other squares
+//     for (var i = 0; i < this.squares.length; i++) {
+//         if (object !== this.squares[i]) {
+//             var isCol = this.rectIntersect(
+//                 object.x,
+//                 object.y,
+//                 object.width,
+//                 object.height,
+//                 this.squares[i].x,
+//                 this.squares[i].y,
+//                 this.squares[i].width,
+//                 this.squares[i].height
+//             );
+//             object.isColliding = isCol;
+//             if (isCol) {
+//                 (this.squares[i]).isColliding = isCol;
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+
+// class GameObject {
+//     constructor(context, x, y, vx, vy, width, height) {
+//         this.context = context;
+//         this.x = x;
+//         this.y = y;
+//         this.vx = vx;
+//         this.vy = vy;
+//         this.width = width;
+//         this.height = height;
+
+//         this.isColliding = false;
+
+//         this.draw = this.draw.bind(this);
+//         this.update = this.update.bind(this);
+//         this.getRight = this.getRight.bind(this);
+//         this.getBottom = this.getBottom.bind(this);
+//         this.setVelocity = this.setVelocity.bind(this);
+//         this.offsetVelocity = this.offsetVelocity.bind(this);
+//     }
+//     getRight() {
+//         return (this.x + this.width);
+//     }
+
+//     getBottom() {
+//         return (this.y + this.height);
+//     }
+
+//     draw(context) { };
+//     update(secondsPassed) { };
+// }
+
+// class Square extends GameObject {
+//     constructor(context, x, y, vx, vy, width, height) {
+//         super(context, x, y, vx, vy);
+
+//         // Set default width and height
+//         this.width = width;
+//         this.height = height;
+
+//         this.draw = this.draw.bind(this);
+//         this.update = this.update.bind(this);
+//         this.setVelocity = this.setVelocity.bind(this);
+//         this.offsetVelocity = this.offsetVelocity.bind(this);
+//     }
+
+//     draw(ctx) {
+//         super.draw(ctx);
+//         // Draw a simple square
+//         ctx.fillStyle = this.isColliding ? '#ff8080' : '#0099b0';
+//         ctx.fillRect(this.x, this.y, this.width, this.height);
+//     }
+
+//     update(secondsPassed) {
+//         super.update(secondsPassed);
+//         // Move with set velocity
+//         this.x += this.vx * secondsPassed;
+//         this.y += this.vy * secondsPassed;
+//         //console.log(this.x + ", " + this.y);
+//     }
+
+//     setVelocity(vx, vy) {
+//         this.vx = vx;
+//         this.vy = vy;
+//     }
+
+//     offsetVelocity(vx, vy) {
+//         this.vx += vx;
+//         this.vy += vy;
+//     }
+// }
+
+// class GameEngine {
+
+//     constructor(canvas, window) {
+//         this.canvas = canvas;
+//         this.context = this.canvas.getContext('2d');
+//         this.window = window;
+
+//         this.secondsPassed = 0;
+//         this.oldTimeStamp = 0;
+//         this.fps = 0;
+
+//         this.squaresize = 100;
+//         this.squares = new Array();
+
+//         this.gameLoop = this.gameLoop.bind(this);
+//         this.update = this.update.bind(this);
+//         this.draw = this.draw.bind(this);
+//         this.doMouseDown = this.doMouseDown.bind(this);
+//         this.doKeyDown = this.doKeyDown.bind(this);
+//         this.checkCollisions = this.checkCollisions.bind(this);
+//         this.checkWallCollisions = this.checkWallCollisions.bind(this);
+//         this.checkObjectCollisions = this.checkObjectCollisions.bind(this);
+//         this.rectIntersect = this.rectIntersect.bind(this);
+//         this.getX = this.getX.bind(this);
+//         this.getY = this.getY.bind(this);
+//         this.getRandomInRange = this.getRandomInRange.bind(this);
+
+//         var temp = this;
+//         this.canvas.addEventListener("mousedown", function (e) { temp.doMouseDown(e) }, false);
+//         this.canvas.addEventListener("keydown", function (e) { temp.doKeyDown(e) }, false);
+
+//         this.window.requestAnimationFrame(this.gameLoop);
+//     }
+
+//     gameLoop(timeStamp) {
+
+//         // Calculate the number of seconds passed since the last frame
+//         this.secondsPassed = (timeStamp - this.oldTimeStamp) / 1000;
+//         this.oldTimeStamp = timeStamp;
+
+//         // Calculate fps
+//         this.fps = Math.round(1 / this.secondsPassed);
+//         this.update(this.secondsPassed);
+//         // Perform the drawing operation
+//         this.draw();
+
+//         // The loop function has reached it's end. Request a new frame
+//         this.window.requestAnimationFrame(this.gameLoop);
+//     }
+
+//     update(secondsPassed) {
+//         this.checkCollisions();
+//         for (var i = 0; i < this.squares.length; i++) {
+//             (this.squares[i]).update(secondsPassed);
+//         }
+//     }
+
+//     draw() {
+//         this.context.clearRect(0, 0, this.canvas.clientWidth, this.canvas.clientHeight);
+
+//         for (var i = 0; i < this.squares.length; i++) {
+//             this.squares[i].draw(this.context);
+//         }
+
+//         // Draw FPS counter to the screen
+//         this.context.fillStyle = 'white';
+//         this.context.fillRect(0, 0, 120, 50);
+//         this.context.font = '25px Arial';
+//         this.context.fillStyle = 'black';
+//         this.context.fillText("FPS: " + this.fps, 10, 30);
+//         /////////////////////////////////
+//     }
+
+//     doMouseDown(event) {
+//         console.log(this);
+//         switch (event.button) {
+//             case 0:
+//                 var rect = this.canvas.getBoundingClientRect();
+//                 this.squares.push(
+//                     new Square(
+//                         null,
+//                         this.getX(event),
+//                         this.getY(event),
+//                         this.getRandomInRange(-50, 50),
+//                         this.getRandomInRange(-50, 50),
+//                         this.squaresize,
+//                         this.squaresize
+//                     )
+//                 )
+//                 console.log(this.squares);
+//                 break;
+
+//             default:
+//                 break;
+//         }
+//     }
+
+//     doKeyDown(event) {
+//         switch (event.key) {
+//             case "ArrowUp":
+//                 // call method for up action
+//                 break;
+//             case "ArrowDown":
+//                 // call method for down action
+//                 break;
+//             default:
+//                 // default action, fallback
+//                 break;
+//         }
+//     }
+
+//     checkCollisions() {
+//         for (var i = 0; i < this.squares.length; i++) {
+//             // for each square, check the wall overlaps
+//             this.checkWallCollisions(this.squares[i]);
+//             // for each square, check for overlaps with other squares
+//             this.checkObjectCollisions(this.squares[i], i);
+//         }
+
+//     }
+
+//     checkWallCollisions(object) {
+//         // check right and left wall overlap
+//         if (object.getRight() >= this.canvas.clientWidth) {
+//             object.vx = -object.vx;
+//             object.x = this.canvas.clientWidth - object.width - 1;
+//         }
+//         else if (object.x <= 0) {
+//             object.vx = -object.vx;
+//             object.x = 1;
+//         }
+
+//         // check bottom and top wall overlap
+//         if (object.getBottom() >= this.canvas.clientHeight) {
+//             object.vy = -object.vy;
+//             object.y = this.canvas.clientHeight - object.height - 1;;
+//         }
+//         else if (object.y <= 0) {
+//             object.vy = -object.vy;
+//             object.y = 1;
+//         }
+//     }
+
+//     checkObjectCollisions(object, index) {
+//         // check parameter square against all other squares
+//         for (var i = 0; i < this.squares.length; i++) {
+//             if (object !== this.squares[i]) {
+//                 var isCol = this.rectIntersect(
+//                     object.x,
+//                     object.y,
+//                     object.width,
+//                     object.height,
+//                     this.squares[i].x,
+//                     this.squares[i].y,
+//                     this.squares[i].width,
+//                     this.squares[i].height
+//                 );
+//                 object.isColliding = isCol;
+//                 if (isCol) {
+//                     (this.squares[i]).isColliding = isCol;
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+
+//     rectIntersect(x1, y1, w1, h1, x2, y2, w2, h2) {
+//         // Check x and y for overlap
+//         if (x2 > w1 + x1 || x1 > w2 + x2 || y2 > h1 + y1 || y1 > h2 + y2) {
+//             return false;
+//         }
+//         return true;
+//     }
+
+//     getX(event) {
+//         var rect = this.canvas.getBoundingClientRect();
+//         return event.clientX - rect.left;
+//     }
+
+//     getY(event) {
+//         var rect = this.canvas.getBoundingClientRect();
+//         return event.clientY - rect.top;
+//     }
+
+//     getRandomInRange(min, max) {
+//         return Math.random() * (Math.abs(min) + max) + min;
+//     }
+// }
